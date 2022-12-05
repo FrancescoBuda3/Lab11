67196aefd1eff74332c0e4f676efb6d76485965b
Francesco Buda - francesco.buda3@studio.unibo.it <francesco.buda3@studio.unibo.it>
2022-12-05 12:56:51 +0100

    completato es 2

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ComplexAlgebra
 {
     /// <summary>
@@ -17,6 +19,42 @@ namespace ComplexAlgebra
     /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        public double Real { get;}
+        public double Imaginary { get;}
+
+        public double Modulus
+        {
+            get => Math.Sqrt(Real * Real + Imaginary * Imaginary);
+        }
+
+        public double Phase
+        {
+            get => Math.Atan2(Imaginary, Real);
+        }
+
+        public Complex(double real, double imm)
+        {
+            Real = real;
+            Imaginary = imm;            
+        }
+
+        public Complex Complement() => new Complex(Real, -Imaginary);
+        public Complex Plus(Complex a) => new Complex(Real + a.Real, Imaginary + a.Imaginary);
+        public Complex Minus(Complex a) => new Complex(Real - a.Real, Imaginary - a.Imaginary);
+        public override string ToString() => $"{Real} {(Imaginary > 0 ? "+" : "-")} i {Math.Abs(Imaginary)}";
+        public override bool Equals(Object a)
+        {
+            if (a.GetType() == this.GetType())
+            {
+                Complex b = (Complex)a;
+
+                if (Real == b.Real && Imaginary == b.Imaginary)
+                {
+                    return true;
+                }
+            }
+            return false; 
+        }
+        public override int GetHashCode() => HashCode.Combine(Real, Imaginary);
     }
 }
